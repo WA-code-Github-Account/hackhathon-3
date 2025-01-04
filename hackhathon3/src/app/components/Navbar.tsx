b2d8d4b7f687807aaead24da8f90bebc76a785db
A.Siddiqui-coder. <163776495+WA-code-Github-Account@users.noreply.github.com>
2025-01-04 16:34:13 +0500

    Update Navbar.tsx

--- hackhathon3/src/app/components/Navbar.tsx
+++ hackhathon3/src/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className='w-full bg-white h-auto flex flex-col md:flex-row items-center justify-between p-4 md:p-8 border-b-2 border-b-[#e7eef6]'>
       <div className="first flex flex-col md:flex-row items-center gap-4 md:gap-16">
-        <h1 className='text-[#4755be] text-4xl font-bold'>WORENT</h1>
+        <h1 className='text-[#4755be] text-4xl font-bold'>MORENT</h1>
         <div className="input relative w-full md:w-auto">
           <Image src={'/search-normal.png'} alt='' width={24} height={24} className='absolute top-1/2 left-3 transform -translate-y-1/2'/>
           <input 
